54856d839dbfc123bb78f64b7fc1218a67a43659
IvoDeK <ivodoetyt@gmail.com>
2021-03-23 21:25:40 +0100

    Created the module with a test script

---
+++ index.js
@@ -0,0 +1,85 @@
+const steamUser = require('steam-user');
+
+const users = new Map();
+
+class user {
+    constructor(accountName, password, gameids) {
+        this.sUser = new steamUser();
+        this.accountName = accountName;
+        this.password = password;
+        this.gameids = gameids;
+    }
+
+    LogIn() {
+        LoginUser(this.sUser, this.accountName, this.password);
+        return this;
+    }
+
+    StartIdle() {
+        Idle(this.sUser, this.accountName, this.password, this.gameids);
+        return this;
+    }
+}
+
+function LoginUser(sUser, accountName, password) {
+    sUser.logOff();
+    sUser.logOn({
+        accountName: accountName,
+        password: password,
+        rememberPassword: true,
+        promptSteamGuardCode: true
+    });
+    sUser.on('error', err => {
+        return undefined;
+    })
+}
+
+function Idle(sUser, accountName, password, gameids) {
+    LoginUser(sUser, accountName, password);
+
+    sUser.on("loggedOn", () => {
+        sUser.setPersona(steamUser.EPersonaState.Online);
+        sUser.gamesPlayed(gameids);
+        console.log(`Idling: ${gameids.length} game${gameids.length > 1? "s":""} on: ${sUser.vanityURL || sUser.steamID}`);
+    });
+}
+
+/**
+ *
+ * @param {string} username - Put your steam username here
+ * @param {string} password - Put your steam password here
+ * @param {number[]} gameids - Put several or one steam game id in here
+ *
+ */
+
+module.exports.setAccount = function({username, password, gameids}) {
+    if (!username || typeof username !== "string") return console.log(`\x1b[5m\x1b[30m\x1b[41m✗\x1b[0m \x1b[31mInvalid username.\x1b[0m`);
+    if (!password || typeof password !== "string") return console.log(`\x1b[5m\x1b[30m\x1b[41m✗\x1b[0m \x1b[31mInvalid password.\x1b[0m`);
+    if (!gameids || gameids.find(id => typeof id !== "number")) return console.log(`\x1b[5m\x1b[30m\x1b[41m✗\x1b[0m \x1b[31mInvalid gameids.\x1b[0m`);
+    if (users.has(username)) return console.log(`\x1b[5m\x1b[30m\x1b[41m✗\x1b[0m \x1b[31mThis account already exists.\x1b[0m`);
+    users[username] = new user(username, password, gameids);
+    users[username].LogIn();
+    return console.log(`\x1b[5m\x1b[30m\x1b[42m✓\x1b[0m \x1b[32mAdded ${username}'s account!\x1b[0m`);
+}
+
+/**
+ *
+ * @param {string} username - Put a steam username here
+ *
+ */
+
+module.exports.logIn = function(username) {
+    if (users[username]) return users[username].LogIn();
+    return console.log(`\x1b[5m\x1b[30m\x1b[41m✗\x1b[0m \x1b[31mNo account could be found.\x1b[0m`);
+}
+
+/**
+ *
+ * @param {string} username - Put a steam username here
+ *
+ */
+
+module.exports.startClient = function(username) {
+    if (users[username]) return users[username].StartIdle();
+    return console.log(`\x1b[5m\x1b[30m\x1b[41m✗\x1b[0m \x1b[31mNo account could be found.\x1b[0m`);
+}
---
+++ test.js
@@ -0,0 +1,11 @@
+const idle = require(process.cwd()+"/index.js");
+
+idle.setAccount({
+    username: process.env.username,
+    password: process.env.pass,
+    gameids: [286570, 312150, 352520, 436150, 444090, 533300, 562220, 566160, 591420, 601220, 607880, 607890, 617670, 626690, 638650, 645090, 654900, 658560, 724740, 757330, 758100, 799070, 806140, 840720, 843890, 864300, 943700, 966630, 1054790, 1091470, 1098340, 1177070]
+});
+
+idle.logIn(process.env.username);
+
+idle.startClient(process.env.username);
